c2de80a69cacb12d8caf98d4bbeb9fc670c40e0d
zaid <54908850+shin-wolf@users.noreply.github.com>
2020-10-03 06:58:43 +0000

    Update scripty.js

--- scripty.js
+++ scripty.js
@@ -2,6 +2,6 @@ var nilaiSantri = [90, 60, 65, 97, 99, 60, 53, 91, 89];
 var jumlah = 0;
 for (i = 0; i < nilaiSantri.length; i++) {
   document.write(nilaiSantri[i]);
-  jumlah += nilaiSantri.length;
+  jumlah += nilaiSantri[i];
 }
 document.write(jumlah / nilaiSantri.length);
